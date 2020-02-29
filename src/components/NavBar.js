8c4d8521f06cf7d89e43cb3756b3171c4606a8fc
AlexFox1777 <mix.filipov@mail.ru>
2020-02-28 22:19:24 -0800

    current state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react'
 
-class NavBar extends Component {
-    render() {
-        return(
-            <div>
-                nav
-            </div>
-        )
-    }
+// class NavBar extends Component {
+//
+//     render() {
+//         console.log("Hello");
+//         return(
+//             <div>
+//                 nav
+//             </div>
+//         )
+//     }
+// }
+
+function NavBar(){
+    const [state, setState] = React.useState(1);
+    console.log(`STATE: ${state}`);
+
+    React.useEffect(() => {
+        // setState(state + 1);
+        console.log(`IN EFFECT`)
+    });
+
+    return(
+        <div>
+            {console.log(`IN DOM`)}
+            NavBar {state}
+            <button onClick={() => setState(state + 1)}>Click</button>
+        </div>
+    )
 }
 
 export default NavBar
